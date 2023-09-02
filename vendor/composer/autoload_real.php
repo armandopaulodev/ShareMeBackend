b251ab8817e1e01567ee9e06c22b14c679c5c064
Armando Paulo <51958179+armandopaulodev@users.noreply.github.com>
2023-09-02 12:36:10 -0700

    ...

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitbefa5bec84d4063800b53438b93f18f7
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitbefa5bec84d4063800b53438b93f18f7::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
